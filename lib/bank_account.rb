acba2d31aff3152459745a9cde3ff74bb21036a5
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-07 23:34:41 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,9 @@
 class BankAccount
   attr_reader :name
-  attr_accessor :balance
+  attr_accessor :balance, :status
   def initialize(name)
     @name = name
     @balance = 1000.00
+    @status = "open"
   end
 end
