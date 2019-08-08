6da6b2179c834240b72913ed0d6ce7cf91a5acff
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-08 00:00:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid?
+    if valid? == true
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
