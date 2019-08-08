cf775ea85109211e9891419808212ba893bba929
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-08 00:05:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,10 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
+    else
+      "Transaction rejected. Please check your account balance."
     end
   end
+
+
 end
